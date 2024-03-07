f3fc11c05e73f7765fa489fcf57f3cf9adf7420f
dannyfadli <danyfdly95@gmail.com>
2024-03-07 12:40:15 +0700

    prosedure untuk input Jika n kurang dari atau sama dengan 20

--- Pertemuan 3 156/Pertemuan 3 156.cpp
+++ Pertemuan 3 156/Pertemuan 3 156.cpp
@@ -3,3 +3,14 @@ using namespace std;
 
 int a[20];                  //Deklarasi array a dengan ukuran 20
 int n;                      //Deklarasi Variabel n untuk menyimpan banyyaknya elemen pada array
+
+void input() {				//prosedure untuk input
+	while (true) {			//looping
+		cout << "Masukan banyaknya elemen pada Array : ";	//Outputt ke layar
+		cin >> n;			//input dari pengguna
+		if (n <= 20)		//jika n kurang dari atau sama dengan 20
+			break;			//keluar dari loop
+
+	}
+
+}
