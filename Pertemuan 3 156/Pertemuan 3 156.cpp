61f7d44895b99401d827eaa7860abf8159235f12
dannyfadli <danyfdly95@gmail.com>
2024-03-07 12:32:56 +0700

    Add project files.

---
+++ Pertemuan 3 156/Pertemuan 3 156.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+
+int a[20];                  //Deklarasi array a dengan ukuran 20
+int n;                      //Deklarasi Variabel n untuk menyimpan banyyaknya elemen pada array
